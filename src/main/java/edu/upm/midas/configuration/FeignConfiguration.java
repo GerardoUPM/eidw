b482f87fbcd9b940f48e9a5d26e2209a7be1fc7b
Gerardo Lagunes García <gerardolagar@ctb.upm.es>
2018-04-26 10:44:07 +0200

    Actualización de la configuración de FEIGN
    Estos cambios son indispensables para su funcionamiento

--- src/main/java/edu/upm/midas/configuration/FeignConfiguration.java
+++ src/main/java/edu/upm/midas/configuration/FeignConfiguration.java
@@ -1,6 +1,5 @@
 package edu.upm.midas.configuration;
 
-import feign.Logger;
 import feign.Request;
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.context.annotation.Bean;
@@ -9,24 +8,39 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FeignConfiguration {
 
-  @Bean
-  public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
-    return new BasicAuthRequestInterceptor("user", "password");
-  }
+  public static int connectTimeOutMillis = 259200000;
+  public static int readTimeOutMillis = 259200000;
+
+  /*@Bean
+  public Contract feignContract() {
+    return new feign.Contract.Default();
+  }*/
+
 
   @Bean
+  public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+    return new BasicAuthRequestInterceptor("admin", "admin");
+  }
+
+  /*@Bean
   Logger.Level feignLoggerLevel() {
     return Logger.Level.FULL;
   }
 
   public static final int SECONDS = 259200000;
 
+  */
+
+
+  /**
+   * Configuración elemental para consumir servicios Web
+   *
+   * @return
+   */
   @Bean
   public Request.Options options() {
 
-    return new Request.Options(SECONDS, SECONDS);
+    return new Request.Options(connectTimeOutMillis, readTimeOutMillis);
   }
-
-
 
 }
