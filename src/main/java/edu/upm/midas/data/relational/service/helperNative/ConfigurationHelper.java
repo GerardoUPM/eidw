e0e379806477af4c7118a922177cd0c0082567af
Gerardo Lagunes García <gerardolagar@ctb.upm.es>
2018-06-06 11:53:55 +0200

    Actualización del método que guarda las configuraciones por las que se crean las snapshot de edsssdb

--- src/main/java/edu/upm/midas/data/relational/service/helperNative/ConfigurationHelper.java
+++ src/main/java/edu/upm/midas/data/relational/service/helperNative/ConfigurationHelper.java
@@ -1,6 +1,7 @@
 package edu.upm.midas.data.relational.service.helperNative;
 import edu.upm.midas.constants.Constants;
 import edu.upm.midas.data.relational.service.ConfigurationService;
+import edu.upm.midas.data.relational.service.SourceService;
 import edu.upm.midas.utilsservice.UniqueId;
 import edu.upm.midas.utilsservice.UtilDate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,8 @@ public class ConfigurationHelper {
 
     @Autowired
     private ConfigurationService confService;
+    @Autowired
+    private SourceService sourceService;
 
     @Autowired
     private UniqueId uniqueId;
@@ -34,13 +37,16 @@ public class ConfigurationHelper {
     public void insert(String source, Date version, String tool, String json){
         String configurationId = uniqueId.generateConfiguration(source, utilDate.dateFormatyyyMMdd(version));
         //System.out.println(source +" | " + version +" | "+ tool +" | "+ json);
-        confService.insertNative(configurationId, Constants.SOURCE_WIKIPEDIA_CODE, version, tool, json);
+//        String sourceId = (source.equals(Constants.SOURCE_WIKIPEDIA)?Constants.SOURCE_WIKIPEDIA_CODE:Constants.SOURCE_PUBMED);
+        String sourceId = sourceService.findByNameNative(source);
+        confService.insertNative(configurationId, sourceId, version, tool, json);
         System.out.println("Insert configuration ready!...");
     }
 
 
     public void insert(String source, String sourceId, Date version, String tool, String json){
         String configurationId = uniqueId.generateConfiguration(source, utilDate.dateFormatyyyMMdd(version));
+        //System.out.println(source +" | " + sourceId +" | " + version +" | "+ tool +" | "+ json);
         confService.insertNative(configurationId, sourceId, version, tool, json);
         System.out.println("Insert configuration ready!...");
     }
